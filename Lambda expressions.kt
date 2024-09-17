88ac3a28da7ea1cf26f74f3e40c9d4df5e097ee5
pedro7531 <pedrohenrique754@icloud.com>
2024-09-16 23:49:30 -0300

    lambda

---
+++ Lambda expressions.kt
@@ -0,0 +1,71 @@
+fun uppercaseString(text: String): String {
+    return text.uppercase()
+}
+fun main() {
+    println(uppercaseString("hello"))
+    // HELLO
+}
+
+fun main() {
+    val upperCaseString = { text: String -> text.uppercase() }
+    println(upperCaseString("hello"))
+    // HELLO
+}
+
+val numbers = listOf(1, -2, 3, -4, 5, -6)
+
+
+val positives = numbers.filter ({ x -> x > 0 })
+
+val isNegative = { x: Int -> x < 0 }
+val negatives = numbers.filter(isNegative)
+
+println(positives)
+// [1, 3, 5]
+println(negatives)
+// [-2, -4, -6]
+
+val numbers = listOf(1, -2, 3, -4, 5, -6)
+val doubled = numbers.map { x -> x * 2 }
+
+val isTripled = { x: Int -> x * 3 }
+val tripled = numbers.map(isTripled)
+
+println(doubled)
+// [2, -4, 6, -8, 10, -12]
+println(tripled)
+// [3, -6, 9, -12, 15, -18]
+
+
+val upperCaseString: (String) -> String = { text -> text.uppercase() }
+
+fun main() {
+    println(upperCaseString("hello"))
+    // HELLO
+}
+
+fun toSeconds(time: String): (Int) -> Int = when (time) {
+    "hour" -> { value -> value * 60 * 60 }
+    "minute" -> { value -> value * 60 }
+    "second" -> { value -> value }
+    else -> { value -> value }
+}
+
+fun main() {
+    val timesInMinutes = listOf(2, 10, 15, 1)
+    val min2sec = toSeconds("minute")
+    val totalTimeInSeconds = timesInMinutes.map(min2sec).sum()
+    println("Total time is $totalTimeInSeconds secs")
+    // Total time is 1680 secs
+}
+
+println({ text: String -> text.uppercase() }("hello"))
+// HELLO
+
+
+// The initial value is zero. 
+// The operation sums the initial value with every item in the list cumulatively.
+println(listOf(1, 2, 3).fold(0, { x, item -> x + item })) // 6
+
+// Alternatively, in the form of a trailing lambda
+println(listOf(1, 2, 3).fold(0) { x, item -> x + item })  // 6
