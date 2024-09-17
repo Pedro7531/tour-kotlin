031dbe7b16269f5ed0e1be4e77c0da5fde595a7a
pedro7531 <pedrohenrique754@icloud.com>
2024-09-16 23:46:02 -0300

    single

---
+++ Single-expression functions.kt
@@ -0,0 +1,15 @@
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun main() {
+    println(sum(1, 2))
+    // 3
+}
+
+fun sum(x: Int, y: Int) = x + y
+
+fun main() {
+    println(sum(1, 2))
+    // 3
+}
