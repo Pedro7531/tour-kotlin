dc6a321f7904198e586d1d0d85c1bb4a5e5e2d30
pedro7531 <pedrohenrique754@icloud.com>
2024-09-16 23:41:30 -0300

    functions

---
+++ function.kt
@@ -0,0 +1,17 @@
+fun hello() {
+    return println("Hello, world!")
+}
+
+fun main() {
+    hello()
+    // Hello, world!
+}
+
+fun sum(x: Int, y: Int): Int {
+    return x + y
+}
+
+fun main() {
+    println(sum(1, 2))
+    // 3
+}
